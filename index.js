16ddeee52080b8836516442e37245bf7149dba9e
siggebrandt <sigge.brandt567@gmail.com>
2024-11-15 13:45:31 +0100

    Skapat loop för cityBoxes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Recommended: All functions declared here
+
+// Funktion för att kolla om entered city finns
 // funktion som kollar vilken stad som är närmast
 // funktion som kollar vilken stad som är längst bort
 
@@ -7,25 +9,41 @@ const cityTitle = document.querySelector("h2");
 const cityDetails = document.querySelector("h3");
 const closestCity = document.querySelector("#closest");
 const furthestCity = document.querySelector("#furthest");
-const cityBoxes = document.querySelector("#cities");
+const cityBox = document.querySelector("#cities");
 const distanceTable = document.querySelector("#table");
+const tabName = document.head.querySelector("title");
 
 // Recommended: Ask for the city name and then the rest of the code
 const enteredCity = prompt("Vilken stad?");
 
 // Skriver ut citynamned som användaren skriver i prompt
 let cityWasFound = false;
+
 for (cityKey in cities) {
     if (enteredCity == cities[cityKey].name) {
         cityWasFound = true;
         cityTitle.innerHTML += `${enteredCity} (${cities[cityKey].country})`;
+        tabName.innerHTML = enteredCity;
         break; // loopen slutar och behöver inte fortsätta
     }
 } // om staden inte finns i databasen så skrivs det ut och h3 blir tömd
 if (cityWasFound == false) {
     cityTitle.innerHTML += enteredCity + " finns inte i databasen";
     cityDetails.innerHTML = "";
+    tabName.innerHTML = "Not Found"
 }
+
+for (let i = 0; i <= cities.length - 1; i++) {
+    let cityBoxP = document.createElement("p");
+    cityBoxP.classList.add("cityBox");
+    cityBoxP.textContent = cities[i].name;
+    cityBox.appendChild(cityBoxP);
+
+    if (enteredCity == cities[i].name) {
+        cityBoxP.classList.add("target");
+    }
+}
+
 
 // pseudokod
 
@@ -35,5 +53,5 @@ if (cityWasFound == false) {
 - Använd en loop som går igenom hela cities objektet, ock skriver ut alla namnen i id baserad ordning.
 - 2 funktioner för att se den närmsta och längst bort liggande staden
 - 
-- Skapa distance table, grid (39x fr), classer = "cell", "head_row", "head_column", "even_row", "even_col"
+- Skapa distance table, grid (39x fr), classer = "cell", "head_row", "head_column", "even_row", "even_col" 
 */
